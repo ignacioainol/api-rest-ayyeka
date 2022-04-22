3e7253f8ca85dc5876585b16063ac04eb2831766
Ignacio Ainol <iggnaxios@gmail.com>
2022-04-21 20:47:09 -0400

    async promise to create JWT

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,56 +1,11 @@
 const express = require('express');
-const axios = require('axios');
-const { executeCommand } = require('../utils/functions');
-const fetch = require('node-fetch');
+const { getToken } = require('../utils/functions');
 const router = express.Router();
 
-// router.get('/', async (req, res) => {
-//     // const buff = new Buffer.from('E67A93A338214F0680313BEBAE936A2D:0ewTmt23j0TPfaHunhodLh0DLSTH7Fei72cD0x0y8YI=');
-//     const mixKeys = 'E67A93A338214F0680313BEBAE936A2D:0ewTmt23j0TPfaHunhodLh0DLSTH7Fei72cD0x0y8YI=';
-//     const base64data = mixKeys.toString('base64');
-
-//     try {
-//         const createToken = await axios.post('http://restapi.ayyeka.com/auth/token', {
-//             headers: {
-//                 'Authorization': `Basic ${base64data}`,
-//                 'Cache-Control': 'no-cache',
-//                 'Content-Type': 'application/x-www-form-urlencoded',
-//                 'Postman-Token': 'b0ee4f16-ac32-4429-b1e1-c39ee4793444',
-//                 'cache-control': 'no-cache'
-//             },
-//             data: JSON.stringify({ 'grant_type': 'client_credentials' })
-//         });
-
-//         const token = await createToken.data;
-
-//         res.send(token);
-
-//     } catch (error) {
-//         console.log(error);
-//         res.status(401).send(error);
-//     }
-
-
-//     // const response = await fetch('https://restapi.ayyeka.com/auth/token', {
-//     //     method: 'POST',
-//     //     headers: {
-//     //         'Authorization': `Basic ${base64data}`,
-//     //         'Cache-Control': 'no-cache',
-//     //         'Content-Type': 'application/x-www-form-urlencoded',
-//     //         'Postman-Token': 'b0ee4f16-ac32-4429-b1e1-c39ee4793444',
-//     //         'cache-control': 'no-cache'
-//     //     },
-//     //     data: { 'grant_type': 'client_credentials' }
-//     // });
-
-//     // // const data = await response.json();
-
-//     // res.send(response);
-// })
-
+// Generate JWT to get data from Ayyeka
 router.get('/', async (req, res) => {
-    await executeCommand();
-    res.send('Token creado');
-})
+    const token = await getToken();
+    res.send(token);
+});
 
 module.exports = router;
--- utils/functions.js
+++ utils/functions.js
@@ -1,59 +1,31 @@
-const axios = require('axios');
 const fs = require('fs');
 
-
-const executeCommand = async () => {
+const getToken = () => {
     const { exec } = require("child_process");
 
-    const args = " -X POST https://restapi.ayyeka.com/auth/token \
-            -H 'Authorization: Basic RTY3QTkzQTMzODIxNEYwNjgwMzEzQkVCQUU5MzZBMkQ6MGV3VG10MjNqMFRQZmFIdW5ob2RMaDBETFNUSDdGZWk3MmNEMHgweThZST0=' \
-            -H 'Cache-Control: no-cache' \
-            -H 'Content-Type: application/x-www-form-urlencoded' \
-            -H 'Postman-Token: b0ee4f16-ac32-4429-b1e1-c39ee4793444' \
-            -H 'cache-control: no-cache' \
-            -d 'grant_type=client_credentials'";
+    return new Promise((resolve, reject) => {
+        const args = " -X POST https://restapi.ayyeka.com/auth/token \
+        -H 'Authorization: Basic RTY3QTkzQTMzODIxNEYwNjgwMzEzQkVCQUU5MzZBMkQ6MGV3VG10MjNqMFRQZmFIdW5ob2RMaDBETFNUSDdGZWk3MmNEMHgweThZST0=' \
+        -H 'Cache-Control: no-cache' \
+        -H 'Content-Type: application/x-www-form-urlencoded' \
+        -H 'Postman-Token: b0ee4f16-ac32-4429-b1e1-c39ee4793444' \
+        -H 'cache-control: no-cache' \
+        -d 'grant_type=client_credentials'";
 
-    const valueToken = exec('curl' + args, async function (error, stdout, stderr) {
-        if (error !== null) {
-            console.log('exec error: ' + error);
-        }
-        const token = await JSON.parse(stdout).access_token;
-        fs.writeFile('access_token.json', stdout, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-        console.log(token);
-        return token;
+        exec('curl' + args, function (error, stdout, stderr) {
+            if (error !== null) {
+                console.log('exec error: ' + error);
+            }
+            const token = JSON.parse(stdout).access_token;
+            resolve(token);
+            fs.writeFile('access_token.json', stdout, (err) => {
+                if (err) throw err;
+                console.log('Token Saved!');
+            });
+        })
     })
-
-    return valueToken;
-}
-
-const getToken = async () => {
-    const buff = new Buffer.from('E67A93A338214F0680313BEBAE936A2D:0ewTmt23j0TPfaHunhodLh0DLSTH7Fei72cD0x0y8YI=');
-    const base64data = buff.toString('base64');
-
-    console.log(base64data);
-
-    const createToken = await axios({
-        method: 'POST',
-        url: 'https://restapi.ayyeka.com/auth/token',
-        headers: {
-            Authorization: `Basic ${base64data}`,
-            'Cache-Control': 'no-cache',
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Postman-Token': 'b0ee4f16-ac32-4429-b1e1-c39ee4793444',
-            'cache-control': 'no-cache'
-        },
-        data: { 'grant_type': 'client_credentials' }
-    });
-
-    const { data: token } = await createToken;
-
-    return token;
 }
 
 module.exports = {
-    executeCommand,
     getToken
 }
